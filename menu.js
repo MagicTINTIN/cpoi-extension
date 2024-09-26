d2a93dc1e160b0155bc797a10fdc11001c9235aa
MagicTINTIN <magictintin@proton.me>
2024-09-27 00:54:39 +0200

    feat: all settings are now saved

--- menu.js
+++ menu.js
@@ -94,9 +94,9 @@ document.getElementById("dataInput").focus();
 document.getElementById("autoOutput").style.transform = "scale(0)";
 
 function home() {
-    document.getElementById("easySection").style.display = "block";
+    document.getElementById("easySection").style.display = localSettings.mode == "easy" ? "block" : "none";
     document.getElementById("settingsSection").style.display = "none";
-    document.getElementById("advancedSection").style.display = "none";
+    document.getElementById("advancedSection").style.display = localSettings.mode == "easy" ? "none" : "block";
 }
 
 
@@ -108,22 +108,61 @@ function switchLanguage() {
     getAll(updateAll);
 }
 
+document.getElementById("settingTheme").addEventListener("click", switchTheme)
+function switchTheme() {
+    let kv = {};
+    kv["theme"] = localSettings.theme == "dark" ? "light" : "dark";
+    set(kv)
+    getAll(updateAll);
+}
+
+document.getElementById("settingMode").addEventListener("click", switchMode)
+function switchMode() {
+    let kv = {};
+    kv["mode"] = localSettings.mode == "easy" ? "classic" : "easy";
+    set(kv)
+    getAll(updateAll);
+}
+
+function saveInstance() {
+    if (localSettings.instance == document.getElementById("settingInstance").value)
+        return;
+    let kv = {};
+    kv["instance"] = document.getElementById("settingInstance").value;
+    set(kv)
+    getAll(updateAll);
+}
+
 function updtLang() {
+    // general interface
     document.getElementById("lang").innerHTML = localSettings.lang == "fr" ? "FR" : "EN";
     document.getElementById("cButton").innerHTML = localSettings.lang == "fr" ? "Copier" : "Copy";
     document.getElementById("pButton").innerHTML = localSettings.lang == "fr" ? "Coller" : "Paste";
     document.getElementById("aButton").innerHTML = localSettings.lang == "fr" ? "Copier/Coller" : "Copy/Paste";
+
+    // settings
     document.getElementById("settingsH4").innerHTML = localSettings.lang == "fr" ? "Paramètres" : "Settings";
+    document.getElementById("settingTheme").innerHTML = localSettings.lang == "fr" ? (localSettings.theme == "dark" ? "Sombre" : "Clair") : (localSettings.theme == "dark" ? "Dark" : "Light");
+    document.getElementById("settingMode").innerHTML = localSettings.lang == "fr" ? (localSettings.mode == "easy" ? "Simplifié" : "Classique") : (localSettings.mode == "easy" ? "Simplified" : "Classic");
+
+    document.getElementById("saveSettings").innerHTML = localSettings.lang == "fr" ? "Enregistrer" : "Save";
+
     // document.getElementById("cancelSettings").innerHTML = localSettings.lang == "fr" ? "Annuler" : "Cancel";
     document.getElementById("saveSettings").innerHTML = localSettings.lang == "fr" ? "Enregistrer" : "Save";
+
+    // inputs/outputs
     document.getElementById("autoInput").placeholder = localSettings.lang == "fr" ? "Entrez votre texte à copier ou votre code ici" : "Enter your text to copy or your code here";
     document.getElementById("dataInput").placeholder = localSettings.lang == "fr" ? "Entrez votre texte à copier ici" : "Enter your text to copy here";
     document.getElementById("codeInput").placeholder = localSettings.lang == "fr" ? "Entrez votre code de presse papier ici" : "Enter your clipboard code here";
+
+    // settings instance value (not really language dependant)
+    document.getElementById("settingInstance").value = localSettings.instance;
 }
 
 let inSettings = false;
 document.getElementById("settings").addEventListener("click", () => {
     if (inSettings) {
+        saveInstance();
         inSettings = false;
         home();
     } else {
@@ -143,6 +182,7 @@ document.getElementById("settings").addEventListener("click", () => {
 // });
 
 document.getElementById("saveSettings").addEventListener("click", () => {
+    saveInstance();
     inSettings = false;
     home();
 });
