abfa78b499d359ba67693d113c5ad888c70b8f25
MagicTINTIN <magictintin@proton.me>
2024-09-27 19:18:24 +0200

    refactor: split in more files + moving to folders

---
+++ src/scripts/menu.js
@@ -0,0 +1,148 @@
+function urlEncode(input) {
+    return input.split('').map(c => {
+        if (/[a-zA-Z0-9\-_.~?]/.test(c)) {
+            return c;
+        } else {
+            return '%' + encodeURIComponent(c).slice(1).toUpperCase();
+        }
+    }).join('');
+}
+
+// COMMINUCATION FUNCTIONS
+
+function updateAndClipboardCopy(obj, value) {
+    console.log(value);
+    obj.value = value;
+    navigator.clipboard.writeText(value);
+}
+
+function getCodeFromCPOI(ret, mode, content, lang = 'en') {
+    if (content == '' || mode == '') return "Input not filled";
+    fetch(`${localSettings.instance}?${lang}&${mode}=${urlEncode(content)}`)
+        .then(response => response.text())
+        .then(text => {
+            updateAndClipboardCopy(ret, text.slice(1));
+        });
+}
+
+function getClipboardFromCPOI(ret, code) {
+    if (code == '') return "Code not filled";
+    fetch(`${localSettings.instance}?p=${code}`)
+        .then(response => response.text())
+        .then(text => {
+            updateAndClipboardCopy(ret, text.slice(1));
+        });
+}
+
+
+function getEasyFromCPOI(ret, content, lang = 'en') {
+    if (content == '') return "Input not filled";
+    fetch(`${localSettings.instance}?${lang}&e=${urlEncode(content)}`)
+        .then(response => response.text())
+        .then(text => {
+            updateAndClipboardCopy(ret, text.slice(1));
+        });
+}
+
+document.getElementById("autoOutput").style.transform = "scale(0)";
+
+// COPY PASTE BUTTONS
+
+document.getElementById("aButton").addEventListener("click", () => {
+    getEasyFromCPOI(document.getElementById("autoOutput"), document.getElementById("autoInput").value);
+    if (document.getElementById("autoInput").value == '') return;
+    document.getElementById("autoOutput").style.transform = "scale(1)";
+});
+document.getElementById("cButton").addEventListener("click", () => { getCodeFromCPOI(document.getElementById("codeInput"), 'c', document.getElementById("dataInput").value) });
+document.getElementById("pButton").addEventListener("click", () => { getClipboardFromCPOI(document.getElementById("dataInput"), document.getElementById("codeInput").value) });
+
+function home() {
+    document.getElementById("easySection").style.display = localSettings.mode == "easy" ? "block" : "none";
+    document.getElementById("settingsSection").style.display = "none";
+    document.getElementById("advancedSection").style.display = localSettings.mode == "easy" ? "none" : "block";
+}
+
+// SUBMENUS
+
+let inSettings = false;
+document.getElementById("settings").addEventListener("click", () => {
+    if (inSettings) {
+        saveInstance();
+        inSettings = false;
+        home();
+        autoFocus();
+    } else {
+        inSettings = true;
+        document.getElementById("settingsSection").style.display = "block";
+        document.getElementById("easySection").style.display = "none";
+        document.getElementById("advancedSection").style.display = "none";
+    }
+});
+
+document.getElementById("saveSettings").addEventListener("click", () => {
+    saveInstance();
+    inSettings = false;
+    home();
+    autoFocus();
+});
+
+function autoFocus() {
+    if (localSettings.mode == "easy")
+        document.getElementById("autoInput").focus();
+    else
+        document.getElementById("dataInput").focus();
+}
+
+// KEYBOARD SHORTCUTS
+
+var autoInput = document.getElementById("autoInput");
+var dataInput = document.getElementById("dataInput");
+var codeInput = document.getElementById("codeInput");
+var ctrlPressed = false;
+autoInput.addEventListener("keydown", function (e) {
+    console.log(e.code);
+    
+    if (e.key === "Enter") {
+        if (ctrlPressed)
+            document.getElementById("aButton").click();
+    } else if (e.key === "Control") {
+        ctrlPressed = true;
+    }
+});
+autoInput.addEventListener("keyup", function (e) {
+    if (e.key === "Control") {
+        ctrlPressed = false;
+    }
+});
+
+dataInput.addEventListener("keydown", function (e) {
+    console.log(e.code);
+    
+    if (e.key === "Enter") {
+        if (ctrlPressed)
+            document.getElementById("cButton").click();
+    } else if (e.key === "Control") {
+        ctrlPressed = true;
+    }
+});
+dataInput.addEventListener("keyup", function (e) {
+    if (e.key === "Control") {
+        ctrlPressed = false;
+    }
+});
+
+codeInput.addEventListener("keydown", function (e) {
+    console.log(e.code);
+    
+    if (e.key === "Enter") {
+        if (ctrlPressed)
+            document.getElementById("pButton").click();
+    } else if (e.key === "Control") {
+        ctrlPressed = true;
+    }
+});
+codeInput.addEventListener("keyup", function (e) {
+    if (e.key === "Control") {
+        ctrlPressed = false;
+    }
+});
---
+++ src/scripts/settings.js
@@ -0,0 +1,133 @@
+thisBrowser.storage.onChanged.addListener(function (changes, areaName) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        console.log(`Storage key "${key}" in "${areaName}" changed. Old value: ${oldValue}, new value: ${newValue}`);
+    }
+});
+
+function updtTheme() {
+    if (localSettings.theme == "dark") {
+        document.documentElement.style.setProperty('--r', '#FE3420');
+        document.documentElement.style.setProperty('--rv', '#FF7769');
+        document.documentElement.style.setProperty('--re', '#FE3420');
+        document.documentElement.style.setProperty('--g', '#CEFE20');
+        document.documentElement.style.setProperty('--gv', '#DCFF69');
+        document.documentElement.style.setProperty('--b', '#207DFE');
+        document.documentElement.style.setProperty('--bt', '#121415');
+        document.documentElement.style.setProperty('--bv', '#4572B1');
+        document.documentElement.style.setProperty('--be', '#4572B1');
+        document.documentElement.style.setProperty('--o', '#FE8020');
+        document.documentElement.style.setProperty('--ot', '#151412');
+        document.documentElement.style.setProperty('--ov', '#FFA969');
+        document.documentElement.style.setProperty('--y', '#FEC220');
+        document.documentElement.style.setProperty('--yv', '#FFD469');
+        document.documentElement.style.setProperty('--n', '#1B1B1E');
+        document.documentElement.style.setProperty('--nno', '#1B1B1E00');
+        document.documentElement.style.setProperty('--nho', '#1B1B1E99');
+        document.documentElement.style.setProperty('--w', '#FBFFFE');
+        document.documentElement.style.setProperty('--nv', '#444146');
+        document.documentElement.style.setProperty('--wv', '#B4B3B6');
+        document.documentElement.style.setProperty('--gray', '#6D676E');
+        document.documentElement.style.setProperty('--bgv', '#333333');
+        document.documentElement.style.setProperty('--bg', '#000000');
+        document.documentElement.style.setProperty('--fg', '#ffffff');
+        document.documentElement.style.setProperty('--fgv', '#cccccc');
+    } else {
+        document.documentElement.style.setProperty('--r', '#FE3420');
+        document.documentElement.style.setProperty('--rv', '#FF7769');
+        document.documentElement.style.setProperty('--re', '#FE3420');
+        document.documentElement.style.setProperty('--g', '#CEFE20');
+        document.documentElement.style.setProperty('--gv', '#DCFF69');
+        document.documentElement.style.setProperty('--b', '#207DFE');
+        document.documentElement.style.setProperty('--bt', '#E1ECFF');
+        document.documentElement.style.setProperty('--bv', '#4572B1');
+        document.documentElement.style.setProperty('--be', '#4572B1');
+        document.documentElement.style.setProperty('--o', '#FE8020');
+        document.documentElement.style.setProperty('--ot', '#FFEDE1');
+        document.documentElement.style.setProperty('--ov', '#FFA969');
+        document.documentElement.style.setProperty('--y', '#FEC220');
+        document.documentElement.style.setProperty('--yv', '#FFD469');
+        document.documentElement.style.setProperty('--n', '#d4d4d4');
+        document.documentElement.style.setProperty('--nno', '#1B1B1E00');
+        document.documentElement.style.setProperty('--nho', '#1B1B1E99');
+        document.documentElement.style.setProperty('--w', '#FBFFFE');
+        document.documentElement.style.setProperty('--nv', '#444146');
+        document.documentElement.style.setProperty('--wv', '#B4B3B6');
+        document.documentElement.style.setProperty('--gray', '#6D676E');
+        document.documentElement.style.setProperty('--fgv', '#333333');
+        document.documentElement.style.setProperty('--fg', '#000000');
+        document.documentElement.style.setProperty('--bg', '#ffffff');
+        document.documentElement.style.setProperty('--bgv', '#eeeeee');
+    }
+}
+
+function updtLang() {
+    // general interface
+    document.getElementById("lang").innerHTML = localSettings.lang == "fr" ? "FR" : "EN";
+    document.getElementById("cButton").innerHTML = localSettings.lang == "fr" ? "Copier" : "Copy";
+    document.getElementById("pButton").innerHTML = localSettings.lang == "fr" ? "Coller" : "Paste";
+    document.getElementById("aButton").innerHTML = localSettings.lang == "fr" ? "Copier/Coller" : "Copy/Paste";
+
+    // settings
+    document.getElementById("settingsH4").innerHTML = localSettings.lang == "fr" ? "Paramètres" : "Settings";
+    document.getElementById("settingTheme").innerHTML = localSettings.lang == "fr" ? (localSettings.theme == "dark" ? "Sombre" : "Clair") : (localSettings.theme == "dark" ? "Dark" : "Light");
+    document.getElementById("settingMode").innerHTML = localSettings.lang == "fr" ? (localSettings.mode == "easy" ? "Simplifié" : "Classique") : (localSettings.mode == "easy" ? "Simplified" : "Classic");
+
+    document.getElementById("saveSettings").innerHTML = localSettings.lang == "fr" ? "Enregistrer" : "Save";
+
+    // inputs/outputs
+    document.getElementById("autoInput").placeholder = localSettings.lang == "fr" ? "Entrez votre texte à copier ou votre code ici" : "Enter your text to copy or your code here";
+    document.getElementById("dataInput").placeholder = localSettings.lang == "fr" ? "Entrez votre texte à copier ici" : "Enter your text to copy here";
+    document.getElementById("codeInput").placeholder = localSettings.lang == "fr" ? "Entrez votre code de presse papier ici" : "Enter your clipboard code here";
+
+    // settings instance value (not really language dependant)
+    document.getElementById("settingInstance").value = localSettings.instance;
+}
+
+document.getElementById("lang").addEventListener("click", switchLanguage)
+function switchLanguage() {
+    let kv = {};
+    kv["lang"] = localSettings.lang == "fr" ? "en" : "fr";
+    set(kv)
+    getAll(updateAll);
+}
+
+document.getElementById("settingTheme").addEventListener("click", switchTheme)
+function switchTheme() {
+    let kv = {};
+    kv["theme"] = localSettings.theme == "dark" ? "light" : "dark";
+    set(kv)
+    getAll(updateAll);
+}
+
+document.getElementById("settingMode").addEventListener("click", switchMode)
+function switchMode() {
+    let kv = {};
+    kv["mode"] = localSettings.mode == "easy" ? "classic" : "easy";
+    set(kv)
+    getAll(updateAll);
+}
+
+function saveInstance() {
+    if (localSettings.instance == document.getElementById("settingInstance").value)
+        return;
+    let kv = {};
+    kv["instance"] = document.getElementById("settingInstance").value;
+    set(kv)
+    getAll(updateAll);
+}
+
+// Initializing and updating all settings
+
+function updateAll() {
+    console.log(localSettings);
+    updtLang();
+    updtTheme();
+}
+
+function initUpdateAll() {
+    updateAll();
+    home();
+    autoFocus();
+}
+
+getAll(initUpdateAll);
