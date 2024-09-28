8107bbc2e2eaf07c5a0350806e435a7fec9c7cae
MagicTINTIN <magictintin@proton.me>
2024-09-28 14:04:31 +0200

    feat: now generating qrcode

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -10,18 +10,27 @@ function urlEncode(input) {
 
 // COMMINUCATION FUNCTIONS
 
-function updateAndClipboardCopy(obj, value) {
+const regex = /^[a-z]{1,6}\-[a-z]{1,6}\-[a-z]{1,6}$/;
+
+function updateAndClipboardCopy(obj, value, isCode = false) {
     console.log(value);
     obj.value = value;
     navigator.clipboard.writeText(value);
+    console.log("isCode? ", isCode);
+    if (isCode) {
+        console.log("isCode");
+        document.getElementById("qrGenButton").style.transform = "scale(1)";
+        new QRCode(document.getElementById("qrcode"), `${localSettings.instance}?qr=1&l=${localSettings.lang}&e=${urlEncode(value)}`);
+        document.getElementById("qrValue").innerHTML = `${localSettings.instance}?qr=1&l=${localSettings.lang}&e=${urlEncode(value)}`;
+    }
 }
 
 function getCodeFromCPOI(ret, mode, content, lang = 'en') {
     if (content == '' || mode == '') return "Input not filled";
-    fetch(`${localSettings.instance}?${lang}&${mode}=${urlEncode(content)}`)
+    fetch(`${localSettings.instance}?l=${lang}&${mode}=${urlEncode(content)}`)
         .then(response => response.text())
         .then(text => {
-            updateAndClipboardCopy(ret, text.slice(1));
+            updateAndClipboardCopy(ret, text.slice(1), true);
         });
 }
 
@@ -38,14 +47,18 @@ function getClipboardFromCPOI(ret, code) {
 function getEasyFromCPOI(ret, content, lang = 'en') {
     if (content == '') return "Input not filled";
     // console.log(`${localSettings.instance}?${lang}&e=${urlEncode(content)}`);
-    fetch(`${localSettings.instance}?${lang}&e=${urlEncode(content)}`)
+    fetch(`${localSettings.instance}?l=${lang}&e=${urlEncode(content)}`)
         .then(response => response.text())
         .then(text => {
-            updateAndClipboardCopy(ret, text.slice(1));
+            if (regex.test(text.slice(1)))
+                updateAndClipboardCopy(ret, text.slice(1), true);
+            else
+                updateAndClipboardCopy(ret, text.slice(1));
         });
 }
 
 document.getElementById("autoOutput").style.transform = "scale(0)";
+document.getElementById("qrGenButton").style.transform = "scale(0)";
 
 // COPY PASTE BUTTONS
 
@@ -74,11 +87,30 @@ document.getElementById("settings").addEventListener("click", () => {
         home();
         autoFocus();
     } else {
+        document.getElementById("qrIcon").src = "../images/qrcode.svg";
         inSettings = true;
+        inQrcode = false;
         document.getElementById("settingsSection").style.display = "block";
         document.getElementById("easySection").style.display = "none";
         document.getElementById("advancedSection").style.display = "none";
         document.getElementById("qrCodeSection").style.display = "none";
+    }
+});
+
+let inQrcode = false;
+document.getElementById("qrGenButton").addEventListener("click", () => {
+    if (inQrcode) {
+        document.getElementById("qrIcon").src = "../images/qrcode.svg";
+        inQrcode = false;
+        home();
+    } else {
+        document.getElementById("qrIcon").src = "../images/cross.svg";
+        inQrcode = true;
+        inSettings = false;
+        document.getElementById("settingsSection").style.display = "none";
+        document.getElementById("easySection").style.display = "none";
+        document.getElementById("advancedSection").style.display = "none";
+        document.getElementById("qrCodeSection").style.display = "block";
     }
 });
 
@@ -103,8 +135,8 @@ var dataInput = document.getElementById("dataInput");
 var codeInput = document.getElementById("codeInput");
 var ctrlPressed = false;
 autoInput.addEventListener("keydown", function (e) {
-    console.log(e.code);
-    
+    // console.log(e.code);
+
     if (e.key === "Enter") {
         if (ctrlPressed)
             document.getElementById("aButton").click();
@@ -119,8 +151,8 @@ autoInput.addEventListener("keyup", function (e) {
 });
 
 dataInput.addEventListener("keydown", function (e) {
-    console.log(e.code);
-    
+    // console.log(e.code);
+
     if (e.key === "Enter") {
         if (ctrlPressed)
             document.getElementById("cButton").click();
@@ -135,8 +167,8 @@ dataInput.addEventListener("keyup", function (e) {
 });
 
 codeInput.addEventListener("keydown", function (e) {
-    console.log(e.code);
-    
+    // console.log(e.code);
+
     if (e.key === "Enter") {
         if (ctrlPressed)
             document.getElementById("pButton").click();
